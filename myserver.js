c5a15ceabb894dca0bcde1637bdf26e75735e8ed
santosh33 <santosh33@gmail.com>
2022-07-01 23:35:05 +0530

    Using Slugs Package for demo

--- myserver.js
+++ myserver.js
@@ -3,11 +3,11 @@ const { count } = require("console");
 const fs = require("fs");
 const http = require('http');
 const url= require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 //! SERVER side code 
  
-
  //* file read in synchronous way 
  const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
  const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
@@ -16,7 +16,10 @@ const replaceTemplate = require('./modules/replaceTemplate');
  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
  const dataObj = JSON.parse(data);
 
+ const slugs = dataObj.map(el => slugify(el.productName,{lower:true}));
+ console.log(slugs);
 
+//! Create Server
 const server = http.createServer((req,res)=> {
   const {query, pathname} = url.parse(req.url, true);
 
