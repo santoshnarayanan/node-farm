5336195354718fde0ce90d10b6b25a528923ee0d
santosh33 <santosh33@gmail.com>
2022-07-01 21:39:07 +0530

    Loading menu item

--- myserver.js
+++ myserver.js
@@ -1,40 +1,60 @@
+const { count } = require("console");
 const fs = require("fs");
 const http = require('http');
 
+//**************************SERVER************************************* */
+ const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName );
+    output = output.replace(/{%IMAGE%}/g, product.image );
+    output = output.replace(/{%PRICE%}/g, product.price );
+    output = output.replace(/{%FROM%}/g, product.from );
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients );
+    output = output.replace(/{%QUANTITY%}/g, product.quantity );
+    output = output.replace(/{%DESCRIPTION%}/g, product.description );
+    output = output.replace(/{%ID%}/g, product.id );
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic' );
+    return output;
+ }
+
  //file read in synchronous way 
+ const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
- const productData = JSON.parse(data);
+ const dataObj = JSON.parse(data);
 
-//#region create server
+//****************************create server*************  */
 const server = http.createServer((req,res)=> {
-  console.log(req.url);
-
   const pathName = req.url;
+
+  //Overview Page
   if(pathName === '/overview' || pathName === '/'){
-    res.end('This is the overview or at root');
+    res.writeHead(200,{'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+    
+    //sending response
+    res.end(output);
+
+    //Product Page
   }else if(pathName === '/product'){
     res.end('This is the product');
+
+    //API
   }else if(pathName === '/api'){
-
-    //#region below code is commented for asynchronous way
-    // fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(err,data)=>{
-    //   const productData = JSON.parse(data);
-    //   console.log(productData);
-    //   res.writeHead(200,{'Content-type': 'application/json' });
-    //   res.end(data);
-    //#endregion
-
     res.writeHead(200,{'Content-type': 'application/json' });
     res.end(data);
-    // });
-  }else {
+   }else {
     res.writeHead(404,{
       'Content-type':'text/html'
     });
     res.end('<h1>Page not found</h1>');
   }
 });
-//#endregion
+
 
 
 //#region listening server
